d7b79d62b62ac99464027bb2b118f68943201990
Josh King <josh.king@monash.edu>
2020-09-14 15:20:27 +1000

    Add documentation for calc

--- calc.py
+++ calc.py
@@ -1,3 +1,7 @@
+# Perform simple arithmetic encoded in an input string:
+# '1 + 2' -> 3, or '1 - 2' -> -1.
+
+
 def compute(expression):
     values = expression.split(' ')
     num0 = int(values[0])
